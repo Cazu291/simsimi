af38f7e600da4bf3c05836ec7b1b184611aecf1a
Eugene <eu.bailliet@gmail.com>
2023-05-07 17:36:03 +0200

    ajout des fichiers simsimi test et simsimi finale (diif?)

---
+++ main/python test simsimi.py
@@ -0,0 +1,29 @@
+﻿def test ():
+    fichier=open("simsimi","a")
+    fichier.write("ce test est reussi\n")
+    fichier.close
+    fichier=open("simsimi","r")
+    for ligne in fichier:
+       print (ligne)
+    fichier.close
+
+def exit ():
+    exit=open("texte","w")
+    exit.write("1ere ligne")
+
+def texte ():
+    question=input("entrez une question")
+    while question!=("stop"):
+        question=input("entrez une question")
+        try:
+            fiche=open("texte","r")
+            for ligne in fiche:
+                if ligne==question:
+                    print(ligne+1)
+            fiche.close
+        except:
+            ecriture=open("texte","a")
+            ecriture.write(question)
+            ecriture.write("\n")
+            ecriture.write(input("entrer la reponse"))
+            ecriture.close
---
+++ main/simsimi finale.py
@@ -0,0 +1,29 @@
+﻿def reset():
+    exit=open("texte","w")
+    exit.write("1ere ligne")
+    exit.write("\n")
+    exit.close
+
+def texte ():
+    question=0
+    reponse=("posez moi une question")
+    while question!=("stop"):
+        fiche=open("texte","r")
+        data=fiche.read()
+        fiche.close()
+        data=data.splitlines()
+        print(data)
+        question=input(reponse)
+        for i,ligne in enumerate(data):
+                if question in ligne:
+                    print("ok")
+                    reponse=(data[i+1])
+                    break
+        if question not in data:
+                    ecriture=open("texte","a")
+                    ecriture.write(question)
+                    ecriture.write("\n")
+                    ecriture.write(input("je ne connais pas cette reponse, que dois-je repondre?"))
+                    ecriture.write("\n")
+                    ecriture.close()
+                    reponse=("repose moi une autre question")
